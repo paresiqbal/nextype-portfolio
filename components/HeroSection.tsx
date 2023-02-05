30c9263535dcad0b458b7762fac6d712e9113d85
paresiqbal <thecrowdel@gmail.com>
2023-02-05 22:15:09 +0700

    first commit

---
+++ components/HeroSection.tsx
@@ -0,0 +1,61 @@
+"use client"; // this is a client component
+import React from "react";
+import Image from "next/image";
+import { Link } from "react-scroll/modules";
+import { HiArrowDown } from "react-icons/hi";
+
+const HeroSection = () => {
+  return (
+    <section id="home">
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+        <div className="md:mt-2 md:w-1/2">
+          <Image
+            src="/me.png"
+            alt=""
+            width={325}
+            height={325}
+            className="rounded-full shadow-2xl"
+          />
+        </div>
+        <div className="md:mt-2 md:w-3/5">
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
+            Hi, saya Iqbal!
+          </h1>
+          <p className="text-lg mt-4 mb-6 md:text-2xl">
+            saya adalah seorang
+            <span className="font-semibold text-teal-600">
+              Freshgraduate | Junior Website Developer | Front End
+            </span>
+            berbasis di Bengkulu, Indonesia. Membuat website yang mampu membuat
+            hidup lebih mudah dan lebih bermakna.
+          </p>
+          <Link
+            to="projects"
+            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700 cursor-pointer"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-row items-center text-center justify-center cursor-pointer">
+        <Link
+          to="about"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        >
+          <HiArrowDown size={35} className="animate-bounce" />
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
